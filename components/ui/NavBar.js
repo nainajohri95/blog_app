1cd769458bdf81f52f9cf2947236263c6b06d206
Naina Johri <jhorinaina8@gmail.com>
2024-12-07 12:29:18 +0530

    pen logo added to the NavBar

--- components/ui/NavBar.js
+++ components/ui/NavBar.js
@@ -22,6 +22,7 @@ const Navbar = () => {
           <h1 className="text-3xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
             BlogSpace
           </h1>
+          <img src="/pen.svg" alt="logo" className="w-8 h-8" />
           <div className="flex gap-4 ml-auto">
             {user ? (
               <div className="flex items-center gap-4">
